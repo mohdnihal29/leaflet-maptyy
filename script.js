5b35deb3b6bcbb9ea83ee6ecd66f7a9eabc6d076
mohdnihal29 <mohdnihal29@gmail.com>
2021-03-14 18:25:16 +1100

    map-loaded

--- script.js
+++ script.js
@@ -3,10 +3,39 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+// const form = ;
+// const containerWorkouts =;
+// const inputType = ;
+// const inputDistance = ;
+// const inputDuration = ;
+// const inputCadence =;
+// const inputElevation = ;
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},13.99z`
+      );
+      const coordinates = [latitude, longitude];
+
+      const map = L.map('map').setView(coordinates, 15);
+      console.log(map);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coordinates)
+        .addTo(map)
+        .bindPopup('Nihals leaflet work :D')
+        .openPopup();
+    },
+    function () {
+      alert('could not get location');
+    }
+  );
